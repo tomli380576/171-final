f683b93d586e8d2c4db4a4a10abb393b71eaf4c6
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-06-01 16:43:16 -0700

    fix state management

--- frontend/171-final-frontend/src/PredictionPage.tsx
+++ frontend/171-final-frontend/src/PredictionPage.tsx
@@ -4,28 +4,25 @@ import { useState } from "react";
 import { ModelInput, getNNPrediction } from './modelAPIs.service';
 import { stateNames } from './stateNames';
 
-const defaultInputs: ModelInput = {
-    state: 'California',
-    county: '',
-    population: 0,
-    cases: 0,
-    deaths: 0,
-    GDP: 0
-};
-
 export const PredictionPage: React.FC = () => {
 
-    const [inputs, setInputs] = useState<ModelInput>(defaultInputs);
     const [selectedState, setSelectedState] = useState<string>('California');
+    const [county, setCounty] = useState<string>('');
+    const [population, setPopulation] = useState<number>(0);
+    const [cases, setCases] = useState<number>(0);
+    const [deaths, setDeaths] = useState<number>(0);
+    const [GDP, setGDP] = useState<number>(0);
     const [NNResult, setNNResult] = useState<number | null>(null);
 
-    function handleChange(key: string, value: string | number) {
-        let newInputs: ModelInput = {
-           ...inputs
-        };
-        newInputs[key] = value;
-        console.log(newInputs)
-        setInputs(newInputs);
+    function buildModelInput(): ModelInput {
+        return {
+            state: selectedState,
+            county: county,
+            population: population,
+            cases: cases,
+            deaths: deaths,
+            GDP: GDP
+        }
     }
 
     return (
@@ -37,41 +34,44 @@ export const PredictionPage: React.FC = () => {
         }}>
             {NNResult === null ?
                 <Stack direction='column' spacing={2} sx={{ width: 500 }}>
-                    <TextField id="outlined-basic" label="County Name" variant="outlined" onChange={(e) => handleChange('county', e.target.value)} />
-                    <FormControl sx={{ m: 1, minWidth: 80 }}>
+                    <TextField
+                        id="outlined-basic"
+                        label="County Name"
+                        variant="outlined"
+                        onChange={(e) => setCounty(e.target.value)} />
+                    <FormControl sx={{ minWidth: 80 }}>
                         <InputLabel id="demo-simple-select-autowidth-label">State</InputLabel>
                         <Select
                             label="State"
                             id="demo-simple-select"
                             value={selectedState}
-                            onChange={(e) => {
-                                handleChange('state', e.target.value)
-                                setSelectedState(e.target.value)
-                            }}
+                            onChange={(e) => setSelectedState(e.target.value)}
                         >
                             {stateNames.map((name, index) => <MenuItem key={index} value={name}>{name}</MenuItem>)}
                         </Select>
                     </FormControl>
-                    <TextField type="number" id="outlined-basic" label="Population" variant="outlined" required={true} onChange={(e) => {
-                        handleChange('population', parseInt(e.target.value))
-                    }} />
-                    <TextField type="number" id="outlined-basic" label="Cases" variant="outlined" required={true} onChange={(e) => {
-                        handleChange('cases', parseInt(e.target.value))
-                    }} />
-                    <TextField type="number" id="outlined-basic" label="Deaths" variant="outlined" required={true}
-                        onChange={(e) => {
-                            handleChange('deaths', parseInt(e.target.value))
-                        }} />
-                    <TextField type="number" id="outlined-basic" label="GDP" variant="outlined" required={true}
-                        onChange={(e) => {
-                            handleChange('GDP', parseInt(e.target.value))
-                        }} />
+                    <TextField
+                        type="number" id="outlined-basic"
+                        label="Population"
+                        variant="outlined" required={true}
+                        onChange={(e) => setPopulation(parseInt(e.target.value))} />
+                    <TextField type="number" id="outlined-basic"
+                        label="Cases"
+                        variant="outlined" required={true}
+                        onChange={(e) => setCases(parseInt(e.target.value))} />
+                    <TextField type="number" id="outlined-basic"
+                        label="Deaths"
+                        variant="outlined" required={true}
+                        onChange={(e) => setDeaths(parseInt(e.target.value))} />
+                    <TextField type="number" id="outlined-basic"
+                        label="GDP"
+                        variant="outlined" required={true}
+                        onChange={(e) => setGDP(parseInt(e.target.value))} />
                     <Button variant='contained' onClick={
                         async () => {
-                            getNNPrediction(inputs).then(res => {
+                            getNNPrediction(buildModelInput()).then(res => {
                                 setNNResult(res);
                             });
-
                         }}>
                         Predict with Neural Network
                     </Button>
